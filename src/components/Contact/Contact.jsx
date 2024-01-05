fd7efd12f3c61a464a85f400b33b1fdac50e43fd
Harshini Rajagopal <harshini.rajagopal@servian.com>
2024-01-05 08:48:44 +0530

    Initial Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:harshini.rajagopal@cognizant.com">harshini.rajagopal@cognizant.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/harshini-rajagopal-340a06131/" target="_blank">linkedin.com/harshini-rajagopal</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/Harshini3228" target="_blank">github.com/Harshini3228</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
